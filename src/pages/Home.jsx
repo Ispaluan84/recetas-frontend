cb7223597fc2a11fde26b670cb3f260f5cd9bbc6
Ismael Pastor <ispaluan@gmail.com>
2025-09-09 17:51:10 +0200

    Pequeños Cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,26 @@
-function Home() { 
-    return (
-    <div>Home pública</div> 
-)};
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+import "../styles/Home.scss";
 
-export default Home;
+export default function Home() {
+    const { user } = useContext(AuthContext);
+
+    return (
+        <div className="home-container">
+            <h1>Bienvenido a Recetas App 🍽</h1>
+
+            {user ? (
+                <p className="logged-in-msg">Hola, {user.username}. ¡Explora nuestras recetas!</p>
+            ) : (
+                <>
+                    <p>Esta es una app púbica para compartir y gestionar recetas.</p>
+                    <div className="home-buttons">
+                        <Link to="/login" className="btn">Iniciar Sesión</Link>
+                        <Link to="/register" className="btn">Registrarse</Link>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
